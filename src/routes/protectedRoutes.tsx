248d089ae9f9a5db34597e37a2c962e5d0d13e6b
Ravisha Peiris <gimpeiris.ravisha@gmail.com>
2024-10-27 18:39:55 +0530

    Configure public and protected routes

---
+++ src/routes/protectedRoutes.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+ 
+import useAuth from "../hooks/useAuth";
+ 
+const ProtectedRoutes: React.FC = () => {
+  const isAuth = useAuth();
+  return isAuth ? <Outlet /> : <Navigate replace to="/" />;
+};
+ 
+export default ProtectedRoutes;
--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Transaction } from "../pages/transaction";
+import ProtectedRoutes from "./protectedRoutes";
+import PublicRoutes from "./publicRoutes";
+import ScrollToTop from "../hooks/scrollToTop";
  
 import ComingSoon from "../pages/comingSoon";
 import PageNotFound from "../pages/pageNotFound";
+import { Transaction } from "../pages/transaction";
  
 const Navigator: React.FC = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
-        <Route path="/" element={<ComingSoon />} />
-        <Route path="*" element={<PageNotFound />} />
-        <Route path="/transaction" element={<Transaction />} />
+        {/* Routes which are accessible without authentication */}
+        <Route element={<PublicRoutes />}>
+          <Route path="/" element={<ComingSoon />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+        {/* Routes which are accessible with authentication */}
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/transaction" element={<Transaction />} />
+        </Route>       
       </Routes>
     </BrowserRouter>
   );
