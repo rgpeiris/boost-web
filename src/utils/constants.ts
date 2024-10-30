b5d2c5e80f562dd8c31207c936c269f3a4b4fa00
Ravisha Peiris <gimpeiris.ravisha@gmail.com>
2024-10-30 11:13:14 +0530

    Added custom JS functions, constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+/**
+ * Constant for the masking string prefix.
+ */
+export const MASK_STRING = "**** **** **** ";
---
+++ src/utils/functions.ts
@@ -0,0 +1,15 @@
+import { MASK_STRING } from "./constants";
+
+/**
+ * Mask a string value, revealing only the last four characters.
+ * @param {string} stringValue - The original string to mask.
+ * @returns {string} The masked string or an empty string if input is invalid.
+ */
+export const maskString = (stringValue: string): string => {
+  if (stringValue) {
+    const sliced = stringValue.slice(-4);
+    const mask = MASK_STRING + sliced;
+    return mask;
+  }
+  return "";
+};
